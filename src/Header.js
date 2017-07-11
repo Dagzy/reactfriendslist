00648efa1e7057d7dbe955897be73b06e78fc8b0
Dagzy <quincye.williams@gmail.com>
2017-07-11 09:34:50 -0400

    QW added logo

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import efalogo from './efaLogo.png';
 const Header = () => {
 	return(
 		<nav className="navbar navbar-inverse">
-			<img src="{efalogo}" alt="efalogo" className="logo"/>
+			<img src={efalogo} alt="efalogo" className="logo"/>
 			<h1>Friends List Application</h1>
 		</nav>
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Header from './Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+			<div className="row">
+				<App>
+					<Header /> 
+				</App>
+			</div>,
+	document.getElementById('root')
+);
+
 registerServiceWorker();
